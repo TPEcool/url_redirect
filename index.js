506b4d213d30d85a214a01f9095c07af09231866
electrovoyage <75689188+TPEcool@users.noreply.github.com>
2024-07-29 19:31:52 +0300

    Add successful redirect message

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ function redirectToUrl() {
             || redirect == 'steam://openurl/https://store.steampowered.com'
         ) {
             window.location.href = redirect
-            close()
+            document.body.innerHTML = 'Check Steam! The page has been opened.<br>If you can\'t see the page, try refreshing this page.'
+            return
         } else {
             document.body.innerHTML = 'Non-Steam URL detected, not redirecting. If you believe this is a mistake, please e-mail <a href=mailto:electrovoyagesoftware@gmail.com>electrovoyagesoftware@gmail.com</a>, explaining the issue.<br><br><a href=https://tpecool.github.io>Back to electrovoyage\'s corner</a>'
             return
         }
     }
     //document.body.innerHTML = 'No URL provided.';
-    window.location.href = "https://tpecool.github.io"
+    //window.location.href = "https://tpecool.github.io"
 }
 window.onload = redirectToUrl;
